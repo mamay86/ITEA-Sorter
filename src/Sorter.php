ba7b034ac38f9046dc621443464f4b946bf2c2d2
Roman <karlson2006@ukr.net>
2018-10-08 13:52:29 +0300

    composer

---
+++ src/Sorter.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roma
+ * Date: 08.10.18
+ * Time: 13:44
+ */
+
+namespace App;
+
+class Sorter
+{
+    public function __construct($data) {
+        echo $data;
+    }
+
+    public function sort() {
+
+    }
+}
